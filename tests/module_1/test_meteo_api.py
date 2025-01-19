f4b8cd31f4cd153a9bc31c4d06900afdc45e3208
10alexmarcos <10alexmarcos@gmail.com>
2025-01-19 23:17:00 +0100

    unit test draft

--- tests/module_1/test_meteo_api.py
+++ tests/module_1/test_meteo_api.py
@@ -1,7 +1,9 @@
 """ This is a dummy example to show how to import code from src/ for testing"""
 
-from src.module_1.module_1_meteo_api import main
+# from src.module_1.module_1_meteo_api import main
+
+from src.module_1.draft import inc
 
 
-def test_main():
-    raise NotImplementedError
+def test_inc():
+    assert inc(4) == 5
